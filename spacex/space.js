d07ba3c624c151662999686540a3263aa43af4cd
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-10-08 13:22:36 +0200

    FIX

--- spacex/space.js
+++ spacex/space.js
@@ -1,5 +1,4 @@
-//@ts-check
-
+// @ts-check
 
 const rocket = {
     x:300,
@@ -10,14 +9,16 @@ const rocket = {
 }
 
 let animation;
-function flyttRakett(){
-rocket.vy += 1;
-rocket.y += rocket.vy;
-rocket.x += rocket.vx;
-rocket.div.style.transform=`translate(0${rocket.x}px,${rocket.y}px)`;
+
+function flyttRakett() {
+    rocket.vy += 1;
+    rocket.y += rocket.vy;
+    rocket.x += rocket.vx;
+    rocket.div.style.transform = `translate(${rocket.x}px,${rocket.y}px)`;
 }
 
-export function startSpillet(){
-setInterval(flyttRakett,1000);
-rocet.div = document.getElementById("falcon");
+export function startSpillet() {
+    rocket.div = document.getElementById("falcon");
+    setInterval(flyttRakett,100);
+
 }
