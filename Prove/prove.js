1d1aa1a3e1b8d054094da6911dfea9f2f909a803
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-11-10 16:16:27 +0100

    fix

---
+++ Prove/prove.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { updateMyProperties, thingsWithId, preCalc, sum, sort } from "../lib/Minos.js";
+
+const web = updateMyProperties(ting);
+
+const { bestill, navn, epost,adresse,op,oppo  } = thingsWithId();
+
+
+
+bestill.onclick = () => {
+   bestilli
