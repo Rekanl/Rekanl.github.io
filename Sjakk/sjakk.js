369e2b5d9463a942717fa0bf824992833fff3320
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-11-12 12:14:45 +0100

    dic

--- Sjakk/sjakk.js
+++ Sjakk/sjakk.js
@@ -4,10 +4,17 @@ const web = updateMyProperties ();
 
 const nyttElement = tag => document.createElement (tag);
 
-const  { brett } = thingsWithId
+const  {brett} = thingsWithId
 
 for (let i=0, i<64; i += 1) {
+    let t = Math.trunc(i/8);
     const rute= nyttElement ("div");
     brett.append (rute);
+    if((i+t) % 2 == 0){
+        rute.className = "rute kvit";
+    } else {
+        rute.className = "rute svart";
+    }
+    rute.innerHTML = i;
 
 }
