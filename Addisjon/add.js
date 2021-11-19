08e40e5a08516d94bc7cb0cec44321ea0674965f
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-11-19 11:35:39 +0100

    fix

--- Addisjon/add.js
+++ Addisjon/add.js
@@ -1,15 +1,16 @@
 // @ts-check
 import { updateMyProperties, thingsWithId } from "../lib/Minos.js";
 
-const web = updateMyProperties ();
+const web = updateMyProperties();
 
-const {t1, t2, vis, algoritme, rutenett } = thingsWithId();
+
+const { vis, algoritme, add } = thingsWithId();
 
 vis.onclick = () => {
+
     algoritme.style.opacity = "1";
-    for(let i=0; i<12 ;i++) {
-        const rute = document.createElement("div");
-        rute.className = "rute";
-        rutenett.append(rute);
-    }
+}
+
+add.onclick = () => {
+    web.siffer++;
 }
