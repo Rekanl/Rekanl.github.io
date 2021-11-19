4abb8b14597884cacd0d9eaf420803b0019fd97b
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-11-19 13:43:53 +0100

    fix

--- Addisjon/add.js
+++ Addisjon/add.js
@@ -9,10 +9,42 @@ web.pos = 0;
 const { vis, algoritme, add } = thingsWithId();
 
 vis.onclick = () => {
+    if (Number(web.t1) > 0 && Number(web.t2) > 0) {
+        algoritme.style.opacity = "1";
+        web.siffer = 0;
+        const siffer1 = Array.from(web.t1).reverse();
+         const siffer2 = Array.from(web.t2).reverse();
 
-    algoritme.style.opacity = "1";
+        web.n0 = siffer1[0] || "";
+        web.n1 = siffer1[1] || "";
+        web.n2 = siffer1[2] || "";
+        web.n3 = siffer1[3] || "";
+        web.n4 = siffer1[4] || "";
+       
+        web.m0 = siffer2[0] || "";
+        web.m1 = siffer2[1] || "";
+        web.m2 = siffer2[2] || "";
+        web.m3 = siffer2[3] || "";
+        web.m4 = siffer2[4] || "";
+      
+    }
 }
 
 add.onclick = () => {
+    const s = web.siffer;
+    const nr1 = Number (web ["n"+s])|| 0;
+    const nr2 = Number (web ["m"+s])|| 0;
+    const nr3 = Number (web ["m"+s]) || 0;
+
+    const sum = nr1 + nr2;
+    if (sum > 9) {
+        const sum1 Array.from(String(sum))
+        web ["c"+(s+1)]=1;
+        web ["s"+s]= sum1[1];
+    } else{
+        web ["s"+(s+1)]= 0;  
+        web ["s"+s]= sum;  
+    }
+  
     web.siffer++;
 }
