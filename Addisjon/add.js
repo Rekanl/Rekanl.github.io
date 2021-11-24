a307a5bd68b2421cd2113d9d62853d6b6eaae592
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-11-24 14:32:13 +0100

    fix

--- Addisjon/add.js
+++ Addisjon/add.js
@@ -6,6 +6,7 @@ const web = updateMyProperties();
 web.pos = 0;
 
 
+
 const { vis, algoritme, add } = thingsWithId();
 
 vis.onclick = () => {
@@ -13,38 +14,27 @@ vis.onclick = () => {
         algoritme.style.opacity = "1";
         web.siffer = 0;
         const siffer1 = Array.from(web.t1).reverse();
-         const siffer2 = Array.from(web.t2).reverse();
-
+        const siffer2 = Array.from(web.t2).reverse();
         web.n0 = siffer1[0] || "";
         web.n1 = siffer1[1] || "";
         web.n2 = siffer1[2] || "";
         web.n3 = siffer1[3] || "";
         web.n4 = siffer1[4] || "";
-       
+        
         web.m0 = siffer2[0] || "";
         web.m1 = siffer2[1] || "";
         web.m2 = siffer2[2] || "";
         web.m3 = siffer2[3] || "";
         web.m4 = siffer2[4] || "";
-      
-    }
+
+    }   
 }
 
 add.onclick = () => {
     const s = web.siffer;
-    const nr1 = Number (web ["n"+s])|| 0;
-    const nr2 = Number (web ["m"+s])|| 0;
-    const nr3 = Number (web ["m"+s]) || 0;
-
+    const nr1 = Number(web["n"+s]);  
+    const nr2 = Number(web["m"+s]);
     const sum = nr1 + nr2;
-    if (sum > 9) {
-        const sum1 Array.from(String(sum))
-        web ["c"+(s+1)]=1;
-        web ["s"+s]= sum1[1];
-    } else{
-        web ["s"+(s+1)]= 0;  
-        web ["s"+s]= sum;  
-    }
-  
+    web["s"+s] = sum;  
     web.siffer++;
 }
