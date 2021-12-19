6ccb519ac693481be1a6929039d597d9405fc832
Rekanl <74905816+Rekanl@users.noreply.github.com>
2021-12-19 11:28:50 +0100

    fix

---
+++ pprove/pprove.js
@@ -0,0 +1,34 @@
+// @ts-check
+import { updateMyProperties, thingsWithId } from "../lib/Minos.js";
+
+const web = updateMyProperties();
+
+
+for () {
+    for () {
+      // kode
+      // kode
+    }
+  }
+;
+
+
+const juleTall = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]);
+
+const bilder = ["🤡", "💫", "💥", "🙇‍♀️", "🧞", "👯", "🏇"];
+
+const { kalender } = thingsWithId();
+
+for (let i = 0; i < 24; i++) {
+    const nr = juleTall[i];
+    web.kalender.push({ nr });
+}
+
+kalender.onclick = (e) => {
+    const t = e.target;
+    if (t.className === "luke") {
+        const idx = Math.trunc(Math.random() * 7);
+        t.innerHTML = bilder[idx];
+    }
+}
