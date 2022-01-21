76b2b8c5e52fc623ca83141de26f8eccf7e64107
Rekanl <74905816+Rekanl@users.noreply.github.com>
2022-01-21 14:10:21 +0100

    fix

---
+++ eee/eee.js
@@ -0,0 +1,36 @@
+const alleBrukere = {};
+
+const leggMegTil = (id, navn, etternavn, alder, adresse, tlf) => {
+    
+    alleBrukere[id] = {
+            navn: navn,
+            etternavn: etternavn,
+            alder: alder,  
+            adresse: adresse,
+            tlf: tlf,
+        }
+    }
+
+leggMegTil("01", "Rekan", "Lutfi", 18, "Afghanistan", 6969696969);
+leggMegTil("02", "Nathaniel", "Stensen", 20, "Rikmannsområdet", "penger");
+leggMegTil("03", "Joe", "Jackson", 23, "Westside", 12345678910);
+
+// Liste over alle idene som finnes i alleBrukere
+// ["01", "02", "03"]
+const AlleIds = Object.keys(alleBrukere);
+
+// Tom liste som skal ha navn
+const bareNavn = []
+
+// array.push(obj)
+
+// Går gjennom AlleIds 0..lengden av AlleIds
+for(let i=0; i<AlleIds.length; ++i){
+    // id er id nr i
+    const id = AlleIds[i];
+    // bruker nr id nr i
+    const bruker = alleBrukere[id];
+    bareNavn = navn
+}
+
+console.log(bareNavn);
