af4acb3d1659483520c7961234c17bc8cc9535d8
Rekanl <74905816+Rekanl@users.noreply.github.com>
2022-02-18 19:56:36 +0100

    fix

--- dokumentsystem/app.js
+++ dokumentsystem/app.js
@@ -7,3 +7,9 @@ console.log(antall)
 
 const minefiler = velgFiler("b.pdf c.pdf","doc");
 
+import {...,konflikt} from "./util.js"
+
+// console.log(matsj([1,2][3,4]));
+// console.log(matsj([1,2][1,2]));
+// console.log(matsj([1,2][1,2,3]));
+console.log(konflikt([100,200], [150,300]))
--- dokumentsystem/util.js
+++ dokumentsystem/util.js
@@ -1,45 +1,60 @@
-// @ts-check
+// // @ts-check
 
-export function velgFiler(filer,type) {
-    const ret = [];
-    const liste = filer.split(" ");
-    for (let i=0;i< liste.length; i++) {
-        const fil = liste[i];
-        if (fil.endsWith(type)) {
-            ret.push(fil);
-        }
-    }
-    return ret.join(" ");
+// export function velgFiler(filer,type) {
+//     const ret = [];
+//     const liste = filer.split(" ");
+//     for (let i=0;i< liste.length; i++) {
+//         const fil = liste[i];
+//         if (fil.endsWith(type)) {
+//             ret.push(fil);
+//         }
+//     }
+//     return ret.join(" ");
+// }
+
+// export function linjerMedOrd(text,ord){
+
+//     const liste = text.split("\n");
+//     const ret = [];
+//     for (let i=0;i< liste.length; i++) {
+//         const linje = liste[i];
+//         if (linje.includes(ord)) {
+//             ret.push(linje);
+//         }
+
+//     }
+//     return ret.length;
+// }
+
+
+
+// function matsj(a,b){
+//     if(a.length!==b.length){
+//         return false
+//     }
+//     var like = 0;
+//     //forløkke
+//     for(let i=0; i<a.length;i++){
+//         if(a[i] === b[i]){
+//             like++;
+//         }
+//     }
+//     if(like === a.length) return true
+//     return false
+// }   
+
+// console.log(matsj([1,2], [3,4]))
+
+
+
+ function konflikt (a,b){
+    const [u,v]=a;
+    const[x,y]=b;
+    if(y>x, yx) 
+    return false;
 }
 
-export function linjerMedOrd(text,ord){
-
-    const liste = text.split("\n");
-    const ret = [];
-    for (let i=0;i< liste.length; i++) {
-        const linje = liste[i];
-        if (linje.includes(ord)) {
-            ret.push(linje);
-        }
-
-    }
-    return ret.length;
-}
-
-let start= 0
-for(let i=0; i<a.length; i++){
-  if(a[i] > 0.00001){
-    start = i; 
-    break;
-  }
-}
-return a.slice(start, a.length)  
-
-var i=0
-for (i=a.length; i>0; i-=1){
-if(a[i]>0.0001){
-break
-}
-}
-return a.slice(0,i+1) 
-
+// console.log(konflikt([100,200], [150,300]))
+// console.log(konflikt([100,200], [300,400]))
+// console.log(konflikt([100,200], [200,300]))
+console.log(konflikt([100,200], [80,500]))
